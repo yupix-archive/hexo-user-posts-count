d4153900391eb06e818b521fcdbd172ddfbf54c5
yupix <yupi0982@outlook.jp>
2020-12-22 01:23:30 +0900

    feat: DEBUGログが出てた

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 hexo.extend.helper.register('user_post_count', function(args) {
     var super_hexo = hexo.locals.toObject();
-    console.log(Object.keys(super_hexo.posts.data))
+    //DEBUG console.log(Object.keys(super_hexo.posts.data))
     var post_count = 0
     for(let i in Object.values(super_hexo.posts.data)) {
         const args_author = '' + args
@@ -9,10 +9,10 @@ hexo.extend.helper.register('user_post_count', function(args) {
         if (args_author == Object.values(super_hexo.posts.data[i]['author']).join("")){
             post_count = post_count + 1
         }
-        //console.log(args_author, author)
-        //console.log(Object.values(super_hexo.posts.data[i]['author']).join(""));
+        //DEBUG console.log(args_author, author)
+        //DEBUG console.log(Object.values(super_hexo.posts.data[i]['author']).join(""));
     }
-    console.log(post_count)
+    //DEBUG console.log(post_count)
     return post_count
 });
 
